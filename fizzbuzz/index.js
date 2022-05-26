f9ef90f8a38f508e28fce727c480a43ec355e2b0
bycho91 <bycho1991@gmail.com>
2022-05-25 19:58:56 -0500

    fizz buzz question

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -11,7 +11,29 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    i % 15 === 0
+      ? console.log("fizzbuzz")
+      : i % 3 === 0
+      ? console.log("fizz")
+      : i % 5 === 0
+      ? console.log("buzz")
+      : console.log(i);
+  }
+
+  // for (let i = 1; i <= n; i++) {
+  //   if (i % 15 === 0) {
+  //     console.log("fizzbuzz");
+  //   } else if (i % 3 === 0) {
+  //     console.log("fizz");
+  //   } else if (i % 5 === 0) {
+  //     console.log("buzz");
+  //   } else {
+  //     console.log(i);
+  //   }
+  // }
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
