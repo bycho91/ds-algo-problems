92d77e3a02c41d33b87c4332d278c383cd8bfb86
bycho91 <bycho1991@gmail.com>
2022-06-07 09:47:37 -0500

    linear search

--- linearSearch/index.js
+++ linearSearch/index.js
@@ -6,11 +6,30 @@
 // linearSearchIncludes(foods, "pizza") --> true
 // linearSearchFind(foods, food => food === "pizza") --> "pizza"
 
-function linearSearchIndexOf(arr, val) {}
+function linearSearchIndexOf(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
 
-function linearSearchIncludes(arr, val) {}
+  return -1;
+}
 
-function linearSearchFind(arr, cb) {}
+function linearSearchIncludes(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) return true;
+  }
+  return false;
+}
+
+function linearSearchFind(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i])) return arr[i];
+  }
+
+  return undefined;
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -51,7 +70,7 @@ describe("linearSearchIndexOf()", () => {
   });
 });
 
-describe.skip("linearSearchIncludes()", () => {
+describe("linearSearchIncludes()", () => {
   it("returns true if item found", () => {
     assert.equal(linearSearchIncludes(foods, "pizza"), true);
   });
@@ -60,17 +79,17 @@ describe.skip("linearSearchIncludes()", () => {
   });
 });
 
-describe.skip("linearSearchFind()", () => {
+describe("linearSearchFind()", () => {
   it("returns item back if found", () => {
     const kevin = people[0];
     assert.equal(
-      linearSearchFind(people, person => person.name === "Kevin"),
+      linearSearchFind(people, (person) => person.name === "Kevin"),
       kevin
     );
   });
   it("returns undefined if item NOT found", () => {
     assert.equal(
-      linearSearchFind(people, person => person.name === "Kevinn"),
+      linearSearchFind(people, (person) => person.name === "Kevinn"),
       undefined
     );
   });
