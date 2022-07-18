c0f3dbbb8ac516e5c4c0bd670c8bd1ae48574107
Benjamin Cho <bycho1991@gmail.com>
2022-07-18 12:55:35 -0500

    test 2

--- anagrams/index.js
+++ anagrams/index.js
@@ -1,7 +1,7 @@
 // Check if two strings are anagrams of each other
 // One string is an anagram of another if it uses exact same characters
 // in exact same quantity. Only consider word characters, and make sure the
-// function is case insensitive.
+// function is case insensitive
 // --- Examples
 //   anagrams('heart', 'earth') --> True
 //   anagrams('heart', '  earth') --> True
